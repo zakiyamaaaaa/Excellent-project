4714205b819c261bc447366b07e59c9610eb8dc0
shoichiyamazaki <shoichiyamazaki@shoichiyamazakinoMacBook-Pro.local>
2017-05-26 17:01:55 +0900

    none

--- PHP/updateUserSwipe03.php
+++ PHP/updateUserSwipe03.php
@@ -14,7 +14,7 @@
 	//Connect Database
 	try{
 		$user = "root";
-		$passward = "dreamcometrue";
+		$passward = "**************";
 		$db_name = "prototypedb";
 		$host = "localhost:3306";
 		$dsn = "mysql:host={$host};dbname={$db_name};charset=utf8";
